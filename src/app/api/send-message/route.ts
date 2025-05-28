ccdfb123e7b7568a7b5566740d1ad4bef80cd41b
thakurtushar09 <thakurtushar1309@gmail.com>
2025-05-29 03:11:48 +0530

    adding message api

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,39 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { Message } from "@/model/User";
+
+
+export async function POST(request:Request){
+    await dbConnect();
+    const {username,content} = request.json();
+
+    try {
+        const user=await UserModel.findOne({username});
+
+        if(!user){
+            return Response.json({
+                success:false,
+                message:"User not found"
+            },{status:404})
+        }
+        if(!user.isAcceptingMessage){
+            return Response.json({
+                success:false,
+                message:"User not Accepting messages right now"
+            },{status:403})
+        }
+
+        const newMessage = {content,createdAt:new Date()};
+        user.messages.push(newMessage as Message);
+        await user.save();
+        return Response.json({
+                success:true,
+                message:"Message sent successfully"
+            },{status:200})
+    } catch (error) {
+        return Response.json({
+                success:false,
+                message:"Error while sending messages"
+            },{status:500})
+    }
+}
